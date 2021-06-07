11bbf8469fca1e72abdf6538abae24ae40a0237f
evgenykuzin <32079738+evgenykuzin@users.noreply.github.com>
2021-06-08 00:36:10 +0300

    Delete src/main/resources directory

--- src/main/resources/authAnticatcha.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    let message = {
-        receiver: "antiCaptchaPlugin",
-        data: {'options': {'antiCaptchaApiKey': 'YOUR-ANTI-CAPTCHA-API-KEY'}},
-        type: "setOptions"
-    };
-    window.postMessage(message, window.location.href);
-})();
